5a17b0ec7087f6441a265375f4d70a0a4694e2bb
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2022-04-11 13:27:33 -0700

    sync

---
+++ environment_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Assuming the miniforge virtual environment is active
+# TODO: Add jetson inference installation code
+conda install -y -c conda-forge opencv
+sudo apt-get install v4l-utils
