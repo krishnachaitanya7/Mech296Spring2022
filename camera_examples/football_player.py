0f973a57911877d976dbd8b1c47ac5b382a590eb
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2022-05-03 00:53:19 -0700

    stable

--- camera_examples/football_player.py
+++ camera_examples/football_player.py
@@ -12,18 +12,20 @@ class FootBallPlayer:
     def __init__(self) -> None:
         self.goal_detected = False
         # Setup the GUI
-        # sg.theme("SandyBeach")
-        # # Very basic window.
-        # # Return values using
-        # # automatic-numbered keys
-        # layout = [
-        #     [sg.Text("Please enter the parameters")],
-        #     [sg.Text("Canny Low", size=(15, 1)), sg.InputText()],
-        #     [sg.Text("Canny High", size=(15, 1)), sg.InputText()],
-        #     [sg.Text("Min Line Length", size=(15, 1)), sg.InputText()],
-        #     [sg.Text("Max Line Gap", size=(15, 1)), sg.InputText()],
-        # ]
-        # self.window = sg.Window("Simple data entry window", layout)
+        sg.theme("SandyBeach")
+        # Very basic window.
+        # Return values using
+        # automatic-numbered keys
+        layout = [
+            [sg.Text("Please enter the parameters")],
+            [sg.Text("Canny Low", size=(15, 1)), sg.InputText()],
+            [sg.Text("Canny High", size=(15, 1)), sg.InputText()],
+            [sg.Text("Min Line Length", size=(15, 1)), sg.InputText()],
+            [sg.Text("Max Line Gap", size=(15, 1)), sg.InputText()],
+            [sg.Text("Min Threshold", size=(15, 1)), sg.InputText()],
+            [sg.Text("Max Threshold", size=(15, 1)), sg.InputText()],
+        ]
+        self.window = sg.Window("Simple data entry window", layout)
         self.camera_loop1()
 
     def read_gui_input(self):
@@ -44,8 +46,16 @@ class FootBallPlayer:
         try:
             max_line_gap = int(values[3])
         except:
-            max_line_gap = 4
-        return canny_low, canny_high, min_line_length, max_line_gap
+            max_line_gap = 5
+        try:
+            low_thresh = int(values[4])
+        except:
+            low_thresh = 150
+        try:
+            high_thresh = int(values[5])
+        except:
+            high_thresh = 255
+        return canny_low, canny_high, min_line_length, max_line_gap, low_thresh, high_thresh
 
     def camera_loop1(self):
         window_title = "CSI Camera"
@@ -59,6 +69,7 @@ class FootBallPlayer:
                     # keyCode = cv2.waitKey(1) & 0xFF
                     # if keyCode == 27 or keyCode == ord("q"):
                     #     break
+                    self.detect_bottom_color(img)
                     self.detect_checkerboard(img, draw_img=True)
             finally:
                 cap.release()
@@ -67,7 +78,14 @@ class FootBallPlayer:
             print("Error: Unable to open CSI camera")
 
     def detect_checkerboard(self, img, draw_img=False):
-        canny_low, canny_high, min_line_length, max_line_gap = 190, 200, 100, 4  # self.read_gui_input()
+        (
+            canny_low,
+            canny_high,
+            min_line_length,
+            max_line_gap,
+            low_thresh,
+            high_thresh,
+        ) = self.read_gui_input()  # 190, 200, 100, 4
         # convert to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # find the chessboard corners
@@ -76,15 +94,51 @@ class FootBallPlayer:
         # find hough lines in the image
         lines = cv2.HoughLinesP(img_grayscale_gb_canny, 1, pi / 180, 50, None, min_line_length, max_line_gap)
         # draw the lines on the image
-        if draw_img and lines is not None:
-            for line in lines:
-                for x1, y1, x2, y2 in line:
-                    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.imshow("Hough Lines", img)
-            keyCode = cv2.waitKey(1) & 0xFF
-            # Stop the program on the ESC key or 'q'
-            if keyCode == 27 or keyCode == ord("q"):
-                raise Exception("User stopped program")
+        # if draw_img and lines is not None:
+        #     for line in lines:
+        #         for x1, y1, x2, y2 in line:
+        #             cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        # cv2 find countours
+        _, binary = cv2.threshold(img_grayscale_gb, low_thresh, high_thresh, cv2.THRESH_BINARY)
+        contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # draw countours if detected
+        if draw_img and contours is not None:
+            for contour in contours:
+                cv2.drawContours(img, contour, -1, (0, 255, 0), 2)
+
+        cv2.imshow("Hough Lines", img)
+        keyCode = cv2.waitKey(1) & 0xFF
+        # Stop the program on the ESC key or 'q'
+        if keyCode == 27 or keyCode == ord("q"):
+            raise Exception("User stopped program")
+
+    def detect_bottom_color(self, img):
+        # RGB to HSV
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # define range of blue color in HSV
+        lower_blue = np.array([94, 80, 2])
+        upper_blue = np.array([126, 255, 255])
+        # Threshold the HSV image to get only blue colors
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        # plot the mask image
+        cv2.imshow("Blue Mask", mask)
+        keyCode = cv2.waitKey(1) & 0xFF
+        # Stop the program on the ESC key or 'q'
+        if keyCode == 27 or keyCode == ord("q"):
+            raise Exception("User stopped program")
+
+        # define range of green color in HSV
+        lower_green = np.array([40, 40, 40])
+        upper_green = np.array([70, 255, 255])
+        # Threshold the HSV image to get only green colors
+        mask_green = cv2.inRange(hsv, lower_green, upper_green)
+        # plot the mask image
+        cv2.imshow("Green Mask", mask_green)
+        keyCode = cv2.waitKey(1) & 0xFF
+        # Stop the program on the ESC key or 'q'
+        if keyCode == 27 or keyCode == ord("q"):
+            raise Exception("User stopped program")
+        # print("sup")
 
     def camera_loop(self):
         window_title = "CSI Camera"
